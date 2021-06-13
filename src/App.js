f10a7182641812707d3e1cfda7da66b1d46f3baf
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-13 21:06:30 +0200

    refactor: reorganize project structure
     - Create views directory
     - Handle global imports

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-const App = () => <h1>Hello world</h1>;
-
-export default App;
---
+++ src/views/Root/Root.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Root = () => <h1>Hello world</h1>;
+
+export default Root;
