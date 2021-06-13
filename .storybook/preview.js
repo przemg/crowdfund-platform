bb99e0a28c7566d0c4af13c7759b6347c0cbe75c
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-14 00:37:14 +0200

    feat: add custom viewports to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,30 @@ import GlobalStyle from '../src/theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { mainTheme } from '../src/theme/mainTheme';
 
+const customViewports = {
+  mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '375px',
+      height: '100%',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '100%',
+    },
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1440px',
+      height: '100%',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -10,6 +34,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: { viewports: customViewports },
 };
 
 export const decorators = [
