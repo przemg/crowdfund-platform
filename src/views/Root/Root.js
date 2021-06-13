e7e99bf9aa5831db543af4ab229ba23f12a98fad
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-13 21:16:26 +0200

    feat: add react router

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,12 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const Root = () => <h1>Hello world</h1>;
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={() => <h1>Hello world</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Root;
