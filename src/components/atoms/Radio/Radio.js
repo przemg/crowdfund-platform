b1aaddeace93d0ecd9a018616d4ae387f19ec2f0
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-15 17:12:54 +0200

    style: add transition to Radio atom

--- src/components/atoms/Radio/Radio.js
+++ src/components/atoms/Radio/Radio.js
@@ -10,6 +10,7 @@ const Radio = styled.input.attrs(() => ({
   border: 1px solid ${({ theme }) => theme.colors.gray100};
   box-shadow: inset 0 0 0 4px ${({ theme }) => theme.colors.white};
   cursor: pointer;
+  transition: background-color 0.3s ease-in;
 
   :checked {
     background-color: ${({ theme }) => theme.colors.cyan200};
