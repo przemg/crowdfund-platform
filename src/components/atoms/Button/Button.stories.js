932de77f7b84fc890ff0772677297b94b3767c39
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-16 15:56:43 +0200

    refactor: add Playground to atoms stories

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 import BookmarkIcon from 'assets/icons/icon-bookmark.svg';
+import styled from 'styled-components';
 import Button from '.';
 
 export default {
@@ -10,17 +12,33 @@ export default {
   },
 };
 
-const PrimaryTemplate = (args) => <Button active {...args} />;
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-gap: 12px;
+  width: 300px;
+`;
+
+const PlaygroundTemplate = ({ disabled, active, ...args }) => (
+  <StyledWrapper>
+    <Button disabled={disabled} {...args} />
+    <Button active={active} icon={BookmarkIcon} {...args} />
+  </StyledWrapper>
+);
+const PrimaryTemplate = (args) => <Button {...args} />;
 const IconTemplate = (args) => <Button icon={BookmarkIcon} {...args} />;
 
-export const Primary = PrimaryTemplate.bind({});
+export const Playground = PlaygroundTemplate.bind({});
+Playground.args = {
+  disabled: false,
+  active: false,
+};
 
+export const Primary = PrimaryTemplate.bind({});
 Primary.args = {
   disabled: false,
 };
 
 export const Icon = IconTemplate.bind({});
-
 Icon.args = {
   active: false,
 };
--- src/components/atoms/Input/Input.stories.js
+++ src/components/atoms/Input/Input.stories.js
@@ -1,10 +1,24 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Input from '.';
 
 export default {
   title: 'Atoms/Input',
   component: Input,
 };
+
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-gap: 12px;
+`;
+
+export const Playground = () => (
+  <StyledWrapper>
+    <Input type="text" />
+    <Input type="number" priceLeadingIcon />
+    <Input type="password" passwordShowAction />
+  </StyledWrapper>
+);
 
 export const Normal = () => <Input type="text" />;
 export const Price = () => <Input type="number" priceLeadingIcon />;
