ee529e5c9a461427e362b27fd8e08156d294897e
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-25 19:22:25 +0200

    refactor: rebuild Heading atom
    Move props connected with size to one prop named size and add level
    props to dynamic heading tag change

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -1,36 +1,30 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Heading = styled.h1`
-  font-size: ${({ theme }) => theme.font.headingM};
+const Heading = styled.h1.attrs(({ level, size: sizeProps }) => {
+  const { length, ...sizesSortedByLevel } = ['XL', 'L', 'M', 'S', 'XS', 'XS'];
+  const size = sizeProps || sizesSortedByLevel[level - 1];
+
+  return {
+    as: `h${level}`,
+    size,
+  };
+})`
   font-weight: ${({ theme }) => theme.font.bold};
   color: ${({ theme }) => theme.color.black};
   line-height: 1.8;
   margin: 0;
   max-width: 60ch;
-
-  ${({ extraSmall }) =>
-    extraSmall &&
-    css`
-      font-size: ${({ theme }) => theme.font.headingXS};
-    `}
-
-  ${({ small }) =>
-    small &&
-    css`
-      font-size: ${({ theme }) => theme.font.headingS};
-    `}
-
-  ${({ large }) =>
-    large &&
-    css`
-      font-size: ${({ theme }) => theme.font.headingL};
-    `}
-
-  ${({ extraLarge }) =>
-    extraLarge &&
-    css`
-      font-size: ${({ theme }) => theme.font.headingXL};
-    `}
+  font-size: ${({ size, theme }) => theme.font[`heading${size}`]};
 `;
+
+Heading.propTypes = {
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+  size: PropTypes.oneOf(['XS', 'S', 'M', 'L', 'XL']),
+};
+
+Heading.defaultProps = {
+  level: 1,
+};
 
 export default Heading;
--- src/components/atoms/Heading/Heading.stories.js
+++ src/components/atoms/Heading/Heading.stories.js
@@ -4,31 +4,46 @@ import Heading from '.';
 export default {
   title: 'Atoms/Heading',
   component: Heading,
-  args: {
-    children: 'This is sample heading',
+};
+
+const Template = (args) => <Heading {...args} />;
+
+export const Playground = Template.bind({});
+
+Playground.argTypes = {
+  level: {
+    options: [1, 2, 3, 4, 5, 6],
+    control: { type: 'select' },
+  },
+  size: {
+    options: ['XS', 'S', 'M', 'L', 'XL'],
+    control: { type: 'select' },
   },
 };
 
-const PlaygroundTemplate = (args) => (
+Playground.args = {
+  children: 'This sample content',
+};
+
+export const Default = () => <Heading>This is default heading</Heading>;
+
+export const Levels = () => (
   <>
-    <Heading extraSmall {...args} />
-    <Heading small {...args} />
-    <Heading {...args} />
-    <Heading large {...args} />
-    <Heading extraLarge {...args} />
+    <Heading level={1}>This is level 1 heading</Heading>
+    <Heading level={2}>This is level 2 heading</Heading>
+    <Heading level={3}>This is level 3 heading</Heading>
+    <Heading level={4}>This is level 4 heading</Heading>
+    <Heading level={5}>This is level 5 heading</Heading>
+    <Heading level={6}>This is level 6 heading</Heading>
   </>
 );
 
-const ExtraSmallHeadingTemplate = (args) => <Heading extraSmall {...args} />;
-const SmallHeadingTemplate = (args) => <Heading small {...args} />;
-const NormalHeadingTemplate = (args) => <Heading {...args} />;
-const LargeHeadingTemplate = (args) => <Heading large {...args} />;
-const ExtraLargeHeadingTemplate = (args) => <Heading extraLarge {...args} />;
-
-export const Playground = PlaygroundTemplate.bind({});
-
-export const ExtraSmall = ExtraSmallHeadingTemplate.bind({});
-export const Small = SmallHeadingTemplate.bind({});
-export const Normal = NormalHeadingTemplate.bind({});
-export const Large = LargeHeadingTemplate.bind({});
-export const ExtraLarge = ExtraLargeHeadingTemplate.bind({});
+export const Sizes = () => (
+  <>
+    <Heading size="XL">This is extra large heading</Heading>
+    <Heading size="L">This is large heading</Heading>
+    <Heading size="M">This is medium heading</Heading>
+    <Heading size="S">This is small heading</Heading>
+    <Heading size="XS">This is extra small heading</Heading>
+  </>
+);
