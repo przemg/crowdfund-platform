79843d97185cd1ce64506217df3d09dabbbb0a73
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-06-25 19:55:08 +0200

    refactor: rebuild ProgressBar atom story

--- src/components/atoms/ProgressBar/ProgressBar.stories.js
+++ src/components/atoms/ProgressBar/ProgressBar.stories.js
@@ -4,12 +4,15 @@ import ProgressBar from '.';
 export default {
   title: 'Atoms/ProgressBar',
   component: ProgressBar,
-  args: {
-    max: 100000,
-    value: 65000,
-  },
 };
 
 const Template = (args) => <ProgressBar {...args} />;
 
-export const Normal = Template.bind({});
+export const Playground = Template.bind({});
+
+Playground.args = {
+  max: 100,
+  value: 30,
+};
+
+export const Default = () => <ProgressBar value={50} max={100} />;
