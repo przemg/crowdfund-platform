03ce149262ebf965285d502ab2d3f833e201a1c5
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-07-27 18:53:39 +0200

    feat: add GenericLoadingIndicator organism

---
+++ src/components/organisms/GenericLoadingIndicator/GenericLoadingIndicator.stories.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import GenericLoadingIndicator from '.';
+
+export default {
+  title: 'Organisms/GenericLoadingIndicator',
+  component: GenericLoadingIndicator,
+};
+
+const Template = () => <GenericLoadingIndicator />;
+
+export const Playground = Template.bind({});
+
+export const Default = () => <GenericLoadingIndicator />;
