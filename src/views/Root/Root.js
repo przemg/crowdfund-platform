bd4b96ca61f9f8173475dad282aea954cb20fb2d
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-07-27 20:03:33 +0200

    feat: scope app views down by authentications state

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,17 +1,31 @@
 import * as React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
-import { routes } from 'routes';
-import LoginPage from '../LoginPage';
-import RegisterPage from '../RegisterPage';
+import { useAuth } from 'context/AuthContext';
+import GenericLoadingIndicator from 'components/organisms/GenericLoadingIndicator';
+import CommonApp from './CommonApp';
+
+const UnauthenticatedApp = React.lazy(() => import('./UnauthenticatedApp'));
+const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
+
+const App = () => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <>
+      <CommonApp />
+      <React.Suspense fallback={<GenericLoadingIndicator />}>
+        {isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </React.Suspense>
+    </>
+  );
+};
 
 const Root = () => (
   <MainTemplate>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={() => <p>Hello world</p>} />
-        <Route path={routes.login} component={LoginPage} />
-        <Route path={routes.register} component={RegisterPage} />
+        <App />
       </Switch>
     </BrowserRouter>
   </MainTemplate>
