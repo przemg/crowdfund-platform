7e0f4fe22c83bd562492b3574ba647b8f45e1b57
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-08-29 18:20:35 +0200

    feat: connect DashboardProjectsView with API

--- src/views/DashboardProjectsView/DashboardProjectsView.js
+++ src/views/DashboardProjectsView/DashboardProjectsView.js
@@ -1,34 +1,78 @@
 import * as React from 'react';
-import Card from 'components/molecules/Card';
 import DashboardTemplate from 'templates/DashboardTemplate';
 import { routes } from 'routes';
+import { useFetch } from 'context/FetchContext';
+import { useAuth } from 'context/AuthContext';
+import ProjectCard from 'components/molecules/ProjectCard';
+import Dropdown from 'components/molecules/Dropdown';
+import { generatePath } from 'react-router';
 
-const DashboardProjectsView = () => (
-  <DashboardTemplate>
-    <DashboardTemplate.Section
-      title="Your all projects"
-      action={{ to: routes.dashboard, label: 'Create new project' }}
-    >
-      <Card
-        id="default-card"
-        title="Title of default card"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, odio sed tincidunt sollicitudin, felis metus sagittis mi, vitae tristique ex ligula ut risus. Phasellus nec faucibus turpis."
-        actionButtonLabel="Default action"
-      />
-      <Card
-        id="default-card"
-        title="Title of default card"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, odio sed tincidunt sollicitudin, felis metus sagittis mi, vitae tristique ex ligula ut risus. Phasellus nec faucibus turpis."
-        actionButtonLabel="Default action"
-      />
-      <Card
-        id="default-card"
-        title="Title of default card"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, odio sed tincidunt sollicitudin, felis metus sagittis mi, vitae tristique ex ligula ut risus. Phasellus nec faucibus turpis."
-        actionButtonLabel="Default action"
-      />
-    </DashboardTemplate.Section>
-  </DashboardTemplate>
-);
+const DashboardProjectsView = () => {
+  const [projects, setProjects] = React.useState();
+  const [projectsLoading, setProjectsLoading] = React.useState(true);
+  const { account } = useAuth();
+  const { axiosInstance } = useFetch();
+
+  React.useEffect(() => {
+    const getProjectsData = async () => {
+      try {
+        setProjectsLoading(true);
+
+        const {
+          data: { data },
+        } = await axiosInstance.get(`/accounts/${account._id}/projects`);
+
+        setProjects(data);
+        setProjectsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProjectsData();
+  }, [axiosInstance, account._id]);
+
+  return (
+    <DashboardTemplate>
+      <DashboardTemplate.Section
+        title="Your all projects"
+        action={{ to: routes.dashboard, label: 'Create new project' }}
+      >
+        {projectsLoading
+          ? 'Loding'
+          : projects.map(({ _id, title, about, photo, brandLogo, brandColor }) => (
+              <ProjectCard
+                key={_id}
+                title={title}
+                about={about}
+                photo={photo}
+                brandLogo={brandLogo}
+                brandColor={brandColor}
+                action={
+                  <Dropdown>
+                    <Dropdown.Button>Manage project</Dropdown.Button>
+                    <Dropdown.List>
+                      <Dropdown.Item
+                        type="link"
+                        to={generatePath(routes.projectDetails, { projectId: _id })}
+                      >
+                        Show project
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        type="link"
+                        to={generatePath(routes.editProject, { projectId: _id })}
+                      >
+                        Edit project
+                      </Dropdown.Item>
+                      <Dropdown.Item type="button">Delete project</Dropdown.Item>
+                    </Dropdown.List>
+                  </Dropdown>
+                }
+              />
+            ))}
+      </DashboardTemplate.Section>
+    </DashboardTemplate>
+  );
+};
 
 export default DashboardProjectsView;
